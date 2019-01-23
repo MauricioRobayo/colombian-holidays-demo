9cc2bb6cc015fb243c777bb923314c71e91b00c9
Mauricio Robayo <2121481+archemiro@users.noreply.github.com>
2019-01-22 22:52:43 -0500

    chore: use default locale for prettyDate

--- src/components/PrettyDate.js
+++ src/components/PrettyDate.js
@@ -2,16 +2,17 @@ import React from "react";
 
 const PrettyDate = props => (
   <time dateTime={props.date}>
-    {new Intl.DateTimeFormat("es", {
+    {new Intl.DateTimeFormat(props.locale, {
       weekday: props.weekday,
       year: props.year,
       month: props.month,
       day: props.day
-    }).format(new Date(props.date.replace("-", "/")))}
+    }).format(new Date(`${props.date}T05:00Z`))}
   </time>
 );
 
 PrettyDate.defaultProps = {
+  locale: "default",
   weekday: "long",
   year: "numeric",
   month: "long",
