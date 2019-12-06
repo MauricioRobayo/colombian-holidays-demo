2b6531c5dc42d578a6ad81addcf1159d8d5ec506
Mauricio Robayo <rfmajo@gmail.com>
2019-12-06 16:48:43 -0500

    Updates

---
+++ precache-manifest.d7d8193bc0c5bbb7a8dd4de8c1c6740f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b02cf4a146ac7e7dac3f66ef50aee2",
+    "url": "/festivos-colombia/index.html"
+  },
+  {
+    "revision": "7e911a1fb354f577c4f9",
+    "url": "/festivos-colombia/static/js/2.44926ce0.chunk.js"
+  },
+  {
+    "revision": "9bb4f48984ece53134af",
+    "url": "/festivos-colombia/static/js/main.f716d804.chunk.js"
+  },
+  {
+    "revision": "ef9183c711735afb37b0",
+    "url": "/festivos-colombia/static/js/runtime~main.db6b3721.js"
+  }
+]);
