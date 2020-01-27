ee2d83f8f8cab3fef4f15bac29f4cd0f6c772c85
MauricioRobayo <MauricioRobayo@users.noreply.github.com>
2020-01-27 17:32:44 +0000

    Automated deployment: Mon Jan 27 17:32:44 UTC 2020 68b772cde091593dfc25d7d0414407cb341cfc9f

--- precache-manifest.d2c7f92355868bcda08fd83a4a1e6dd9.js
+++ precache-manifest.d2c7f92355868bcda08fd83a4a1e6dd9.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "844d4624d1f0d191019b53082c4567c1",
+    "revision": "297a734f8532509fee9de8ae3bcb98d8",
     "url": "/colombian-holidays/index.html"
   },
   {
-    "revision": "fe5141c508d785f4746f",
-    "url": "/colombian-holidays/static/js/2.1b0c727e.chunk.js"
+    "revision": "22981199cedf7c8887cc",
+    "url": "/colombian-holidays/static/js/2.8a7ed84e.chunk.js"
   },
   {
     "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
-    "url": "/colombian-holidays/static/js/2.1b0c727e.chunk.js.LICENSE"
+    "url": "/colombian-holidays/static/js/2.8a7ed84e.chunk.js.LICENSE"
   },
   {
     "revision": "ca3c36c0dc134ebbf715",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colombian-holidays/precache-manifest.596fb4e1ab37dc9ff9fbd23d5e8c4c20.js"
+  "/colombian-holidays/precache-manifest.d2c7f92355868bcda08fd83a4a1e6dd9.js"
 );
 
 self.addEventListener('message', (event) => {
