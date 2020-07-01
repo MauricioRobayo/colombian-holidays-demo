b3aa8c05b447c62bfb938b4199077d0fc401b5b4
Mauricio Robayo <rfmajo@gmail.com>
2020-07-01 12:03:56 -0500

    refactor: extract date function to helper file

--- src/containers/Holidays.js
+++ src/containers/Holidays.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
-import { getAllHolidays } from 'pascua'
 import NoMatch from '../components/NoMatch'
 import HolidaysList from '../components/HolidaysList'
 import Day from '../components/Day'
 import NoHolidays from '../components/NoHolidays'
+import {
+  getYears,
+  getMonths,
+  getDays,
+  getHolidays,
+  isValidYear,
+  isValidMonth,
+  isValidDay,
+} from '../utils/dateHelpers'
 
 class HolidaysContainer extends Component {
   startYear = 1984
@@ -14,16 +22,21 @@ class HolidaysContainer extends Component {
     year: this.props.match.params.year,
     month: this.props.match.params.month,
     day: this.props.match.params.day,
-    years: this.getYears(),
-    months: this.getMonths(),
-    days: this.getDays(
-      this.props.match.params.year,
-      this.props.match.params.month,
+    years: getYears(
+      this.currentYear,
+      this.startYear,
+      this.yearsPastCurrentYear,
     ),
-    holidays: this.getHolidays(this.props.match.params.year),
+    months: getMonths(),
+    days: getDays(this.props.match.params.year, this.props.match.params.month),
+    holidays: getHolidays(
+      this.props.match.params.year,
+      this.startYear,
+      this.endYear,
+    ),
   }
 
-  onChangeHandler = event => {
+  onChangeHandler = (event) => {
     const { name, value } = event.target
     this.setState({
       [name]: value,
@@ -37,60 +50,6 @@ class HolidaysContainer extends Component {
     this.props.history.push(path)
   }
 
-  getYears() {
-    const totalYears =
-      this.currentYear - this.startYear + this.yearsPastCurrentYear + 1
-    return Array.from({ length: totalYears }, (_, k) => this.startYear + k)
-  }
-
-  getMonths() {
-    return [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ]
-  }
-
-  getDays(year, month) {
-    if (!year || !month) return []
-    const intYear = parseInt(year, 10)
-    const intMonth = parseInt(month, 10)
-    if (isNaN(intYear) || isNaN(intMonth)) return []
-    const totalDays = new Date(intYear, intMonth, 0).getDate()
-    return Array.from({ length: totalDays }, (_, k) => k + 1)
-  }
-
-  getHolidays(year) {
-    if (!this.isValidYear(year)) {
-      return []
-    }
-    return getAllHolidays(year).sort((a, b) => a.date.localeCompare(b.date))
-  }
-
-  isValidYear(year) {
-    const startYear = parseInt(this.startYear, 10)
-    const endYear = parseInt(this.endYear, 10)
-    return year >= startYear && year <= endYear
-  }
-
-  isValidMonth(month) {
-    const monthInt = parseInt(month, 10)
-    return monthInt >= 1 && monthInt <= 12
-  }
-
-  isValidDay(day) {
-    return this.state.days.includes(parseInt(day, 10))
-  }
-
   componentDidUpdate(prevProps) {
     if (this.props.match.params.year !== prevProps.match.params.year) {
       this.setState({
@@ -101,7 +60,7 @@ class HolidaysContainer extends Component {
     if (this.props.match.params.month !== prevProps.match.params.month) {
       this.setState({
         month: this.props.match.params.month,
-        days: this.getDays(
+        days: getDays(
           this.props.match.params.year,
           this.props.match.params.month,
         ),
@@ -116,9 +75,10 @@ class HolidaysContainer extends Component {
 
   render() {
     if (
-      (this.state.year && !this.isValidYear(this.state.year)) ||
-      (this.state.month && !this.isValidMonth(this.state.month)) ||
-      (this.state.day && !this.isValidDay(this.state.day))
+      (this.state.year &&
+        !isValidYear(this.state.year, this.startYear, this.endYear)) ||
+      (this.state.month && !isValidMonth(this.state.month)) ||
+      (this.state.day && !isValidDay(this.state.day, this.state.days))
     ) {
       const yearsOptions = {
         name: 'year',
@@ -136,7 +96,7 @@ class HolidaysContainer extends Component {
     if (this.state.day) {
       const date = `${this.state.year}-${this.state.month}-${this.state.day}`
       const isHoliday = this.state.holidays.find(
-        holiday => holiday.date === date,
+        (holiday) => holiday.date === date,
       )
       return (
         <Day
@@ -149,7 +109,7 @@ class HolidaysContainer extends Component {
     }
     const holidays = this.state.month
       ? this.state.holidays.filter(
-          holiday => holiday.date.split('-')[1] === this.state.month,
+          (holiday) => holiday.date.split('-')[1] === this.state.month,
         )
       : this.state.holidays
     if (this.state.month && holidays.length === 0) {
